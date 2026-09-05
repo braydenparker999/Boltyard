29df496d62f48d6acb44f83d2b831a47c2c7a931
braydenparker999 <braydenparker999@gmail.com>
2026-09-05 14:21:49 -0300

    Distinguish generated screenshots from bundled resource inputs

--- tests/capture.gd
+++ tests/capture.gd
@@ -4,13 +4,14 @@ func _initialize() -> void:
 	call_deferred("capture")
 
 func capture() -> void:
+	var output_dir = ProjectSettings.globalize_path("res://").path_join("build")
 	var scene = load("res://main.tscn").instantiate()
 	root.add_child(scene)
 	scene.starter()
 	for index in range(90):
 		await process_frame
 	await RenderingServer.frame_post_draw
-	var error = root.get_texture().get_image().save_png("res://build/workshop.png")
+	var error = root.get_texture().get_image().save_png(output_dir.path_join("workshop.png"))
 	if error != OK:
 		push_error("Could not capture workshop")
 		quit(1)
@@ -19,7 +20,7 @@ func capture() -> void:
 	for index in range(90):
 		await process_frame
 	await RenderingServer.frame_post_draw
-	error = root.get_texture().get_image().save_png("res://build/driving.png")
+	error = root.get_texture().get_image().save_png(output_dir.path_join("driving.png"))
 	if error != OK:
 		push_error("Could not capture driving view")
 		quit(1)
